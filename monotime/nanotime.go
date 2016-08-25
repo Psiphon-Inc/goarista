d002785f4c6725d9f62f0e3e7b1e2a20455ed027
Rod Hynes <rod-hynes@users.noreply.github.com>
2016-08-25 02:51:56 -0400

    Add helpers for Psiphon integration * monotime.Time type makes it clear when   a non-"wall" clock time is being used. * functions including Add, Sub, After, and   Before enable drop-in replacement of   time.Now() with monotime.Now() for   duration calculations.

--- monotime/nanotime.go
+++ monotime/nanotime.go
@@ -6,6 +6,7 @@
 package monotime
 
 import (
+	"time"
 	_ "unsafe" // required to use //go:linkname
 )
 
@@ -13,12 +14,45 @@ import (
 //go:linkname nanotime runtime.nanotime
 func nanotime() int64
 
+// Time is a point in time represented in nanoseconds.
+type Time int64
+
 // Now returns the current time in nanoseconds from a monotonic clock.
 // The time returned is based on some arbitrary platform-specific point in the
 // past.  The time returned is guaranteed to increase monotonically at a
 // constant rate, unlike time.Now() from the Go standard library, which may
 // slow down, speed up, jump forward or backward, due to NTP activity or leap
 // seconds.
-func Now() uint64 {
-	return uint64(nanotime())
+func Now() Time {
+	return Time(nanotime())
+}
+
+// Since is analogous to https://golang.org/pkg/time/#Since
+func Since(t Time) time.Duration {
+	return time.Duration(Now() - t)
+}
+
+// Add is analogous to https://golang.org/pkg/time/#Time.Add
+func (t Time) Add(d time.Duration) Time {
+	return t + Time(d)
+}
+
+// Sub is analogous to https://golang.org/pkg/time/#Time.Sub
+func (t Time) Sub(s Time) time.Duration {
+	return time.Duration(t - s)
+}
+
+// Before is analogous to https://golang.org/pkg/time/#Time.Before
+func (t Time) Before(u Time) bool {
+	return t < u
+}
+
+// After is analogous to https://golang.org/pkg/time/#Time.After
+func (t Time) After(u Time) bool {
+	return t > u
+}
+
+// Equal is analogous to https://golang.org/pkg/time/#Time.Equal
+func (t Time) Equal(u Time) bool {
+	return t == u
 }
